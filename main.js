626e7ffee6aac263018ae10fa8fae445666f2775
DuyDo <duydo.work@gmail.com>
2017-08-17 13:07:41 -0500

    reverse string

---
+++ main.js
@@ -0,0 +1,11 @@
+// Excer ofalgorithm scripting on free code camp
+
+// Reverse a String 
+function revStr(str) {
+    str = str.split("");
+    str = str.reverse().join("");
+    console.log(str);
+}
+
+revStr("Hello");
+
