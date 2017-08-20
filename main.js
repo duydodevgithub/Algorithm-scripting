67e1dd2bbd5245709d0be15ef75580b1b1fea526
DuyDo <duydo.work@gmail.com>
2017-08-20 00:55:45 -0500

    factirization number

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ function revStr(str) {
 
 revStr("Hello");
 
+// Factorialize a Number
+/* Return the factorial of the provided integer.
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n. */
+
+
+
