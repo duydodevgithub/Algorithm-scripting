3e42479046e071e8ae3e9482cfbbb98eb0c6a051
DuyDo <duydo.dev@gmail.com>
2017-08-26 18:00:49 -0500

    finish chunkarrayingroups

--- main.js
+++ main.js
@@ -143,4 +143,17 @@ function truncateString(str, num){
 }
 // truncateString("A-", 1);
 
-// ================================== 
+// ================================== Chunky Monkey
+//Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+function chunkArrayInGroups(arr, size) {
+    var newArr = [];
+    for(i = 0; i < arr.length; i+= size) {
+        newArr.push(arr.slice(i, size + i));
+    }
+    console.log(newArr);
+    return newArr;
+}
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
