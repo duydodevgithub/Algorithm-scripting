5b8a8192f4d440544431b95ce278ff0c1eb1cc71
DuyDo <duydo.dev@gmail.com>
2017-08-26 16:39:17 -0500

    finish truncate string

--- main.js
+++ main.js
@@ -130,3 +130,17 @@ function repeatStringNumTimes(str,num) {
 
 // ================================== Truncate a string
 //Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+function truncateString(str, num){ 
+    if(num <= 3) {
+        str = str.substring(0, num);
+        str = str + "...";
+    } else if (num < str.length ) {
+        str = str.substring(0, num - 3);
+        str = str + "...";
+    }
+    console.log(str);
+    return str;
+}
+// truncateString("A-", 1);
+
+// ================================== 
