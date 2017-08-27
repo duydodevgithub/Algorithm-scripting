cafb948a635adb7caf36892eedcc5cadefdae8c8
DuyDo <duydo.dev@gmail.com>
2017-08-27 01:08:08 -0500

    Seek and Destroy

--- main.js
+++ main.js
@@ -197,4 +197,7 @@ function bouncer(arr) {
 
 // bouncer([7, "ate", "", false, 9]);
 
-//==================================
+//==================================Seek and Destroy
+//You will be provided with an initial array (the first argument in the destroyer function), 
+//followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
