48062f0f3acf409596cca158af08808d36306427
DuyDo <duydo.dev@gmail.com>
2017-08-26 21:54:32 -0500

    finish mutation

--- main.js
+++ main.js
@@ -166,4 +166,22 @@ function slasher(arr, howMany) {
 
 // slasher([1, 2, 3], 2)
 
-//==================================
+//==================================Mutations
+//Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+function mutation(arr) {
+    var firstElement = arr[0].toLowerCase();    
+    var secondElement = arr[1].toLowerCase().split("");
+    console.log(firstElement);
+    console.log(secondElement);
+    for(i = 0; i < secondElement.length; i++) {
+        if(firstElement.indexOf(secondElement[i]) === -1) {
+            console.log("false");
+            return false;        
+        }
+    }
+    return true;
+}
+
+// mutation(["Hello", "Heyo"]);
+
