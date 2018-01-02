ddd72bddf765f06e3a8d2b623f8e339d8a4a51a8
DuyDo <duydo.dev@gmail.com>
2018-01-02 01:36:46 -0600

    Smallest Common Multiple

--- main.js
+++ main.js
@@ -541,3 +541,8 @@ function sumPrimes(number) {
 
 //   sumPrimes(10);
 
+//==================================Smallest Common Multiple
+//Find the smallest common multiple of the provided parameters that can be evenly divided by both,
+//as well as by all sequential numbers in the range between these parameters.
+
+
