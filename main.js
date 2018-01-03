4f817d52e7b81a413803232abcb5febaaa456415
DuyDo <duydo.dev@gmail.com>
2018-01-03 14:11:32 -0600

    common multiple

--- main.js
+++ main.js
@@ -546,3 +546,28 @@ function sumPrimes(number) {
 //as well as by all sequential numbers in the range between these parameters.
 
 
+function smallestCommons(arr) {
+    var range = [];
+    for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
+    range.push(i);
+    }
+
+    // can use reduce() in place of this block
+    var lcm = range[0];
+    for (i = 1; i < range.length; i++) {
+    var GCD = gcd(lcm, range[i]);
+    lcm = (lcm * range[i]) / GCD;
+    }
+    return lcm;
+
+    function gcd(x, y) {    // Implements the Euclidean Algorithm
+    if (y === 0)
+        return x;
+    else
+        return gcd(y, x%y);
+    }
+}
+
+// test here
+smallestCommons([1,5]);
+  
