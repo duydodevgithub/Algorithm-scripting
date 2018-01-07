7ab56483bf9c15f1a1fa9527b2b1a4297b1a9cca
DuyDo <duydo.dev@gmail.com>
2018-01-07 01:22:53 -0500

    ..

--- main.js
+++ main.js
@@ -576,10 +576,12 @@ function smallestCommons(arr) {
 
 function findElement(arr, func) {
     var num = 0;
+
     return num;
   }
   
-//   findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+  findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
+  ,
   
 
   
