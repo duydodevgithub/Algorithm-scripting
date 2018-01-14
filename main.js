fe53d075d8572023c1a85ff37ab641f046454339
DuyDo <duydo.dev@gmail.com>
2018-01-13 21:00:01 -0600

    add dropt it algorithm

--- main.js
+++ main.js
@@ -585,6 +585,8 @@ function findElement(arr, func) {
   
 //   findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
 //=====================================================Drop it
+//Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true
+
 
   
 
