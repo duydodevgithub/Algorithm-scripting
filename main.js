4c7317552604be8e5b0e5463199281164f009970
DuyDo <duydo.dev@gmail.com>
2018-01-18 21:35:47 -0600

    finish arguments optional

--- main.js
+++ main.js
@@ -650,9 +650,18 @@ function truthCheck(collection, pre) {
 //========================================================Arguments Optional
 //Create a function that sums two arguments together. If only one argument is provided, then return a function that expects one argument and returns the sum.
 
+// //jshint esversion: 6
 function addTogether() {
-    return false;
+    var args = Array.from(arguments);
+    return args.some(n => typeof n !== 'number') ? 
+      undefined: 
+      args.length > 1 ?
+        args.reduce((acc, n) => acc += n, 0):
+        (n) => typeof n === "number" ? 
+          n + args[0]:
+          undefined;
   }
   
-  addTogether(2,3);
+  // test here
+  addTogether(2,3)
   
