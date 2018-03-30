f1f6d887c480c0a0bf8991266bb83ae72ce2016f
DuyDo <duydo.dev@gmail.com>
2018-03-30 00:22:27 -0500

    record collection

--- main.js
+++ main.js
@@ -662,5 +662,57 @@ function addTogether() {
           undefined;
   }
   // test here
-  addTogether(2,3)
+//   addTogether(2,3)
+
+//========================================================Validate US Telephone Numbers
+function telephoneCheck(str) {
+    var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+    return regex.test(str);
+ }
+//  telephoneCheck("555-555-5555");
+//========================================================Record Collection
+
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+    
+  
+  return collection;
+}
+
+// Alter values below to test your code
+// updateRecords(5439, "artist", "ABBA");
+
+
+ 
+ 
+
   
