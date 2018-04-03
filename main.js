379fad23f682f4f009ab5eee58bbb827d722f821
DuyDo <duydo.dev@gmail.com>
2018-04-03 00:57:38 -0500

    add checkCashRegister algorithm

--- main.js
+++ main.js
@@ -754,7 +754,29 @@ function sym() {
 }
 
 // sym([1, 2, 3], [5, 2, 1, 4]);
+//===================================Exact Change
+//Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
+
+function checkCashRegister(price, cash, cid) {
+    var change;
+    // Here is your change, ma'am.
+    return change;
+  }
+  
+  // Example cash-in-drawer array:
+  // [["PENNY", 1.01],
+  // ["NICKEL", 2.05],
+  // ["DIME", 3.10],
+  // ["QUARTER", 4.25],
+  // ["ONE", 90.00],
+  // ["FIVE", 55.00],
+  // ["TEN", 20.00],
+  // ["TWENTY", 60.00],
+  // ["ONE HUNDRED", 100.00]]
+  
+  checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+  
  
  
 
