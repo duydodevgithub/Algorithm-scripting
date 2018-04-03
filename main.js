2d0fe8a0d29ba6d7681e64d4e506dcc900d0df7f
DuyDo <duydo.dev@gmail.com>
2018-04-03 00:55:52 -0500

    finish symetric diff

--- main.js
+++ main.js
@@ -725,6 +725,35 @@ function updateRecords(id, prop, value) {
 //=======================================Symmetric Difference
 //Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
 
+function sym() {
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+
+    function symDiff(arrayOne, arrayTwo) {
+        var result = [];
+
+        arrayOne.forEach(function(item) {
+            if (arrayTwo.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        arrayTwo.forEach(function(item) {
+            if (arrayOne.indexOf(item) < 0 && result.indexOf(item) < 0) {
+                result.push(item);
+            }
+        });
+
+        return result;
+    }
+
+    // Apply reduce method to args array, using the symDiff function
+    return args.reduce(symDiff);
+}
+
+// sym([1, 2, 3], [5, 2, 1, 4]);
 
  
  
