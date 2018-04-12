e6dd2609eedbb2fea23611c84b339f9a2cf81138
DuyDo <duydo.dev@gmail.com>
2018-04-12 01:41:36 -0500

    finish fizzbuzz

--- udemy_main_algorithm.js
+++ udemy_main_algorithm.js
@@ -1,18 +1,18 @@
 //fizz buzz question
 
-function fizzbuzz(x) {
-    if((x % 3) === 0) {
-        if( x % 5 === 0) {
-            console.log("FizzBuzz");
+function fizzBuzz(n) {
+    for(var i = 1; i <= n; i++) {
+        if(i%3 === 0) {
+            if(i%5 === 0) {
+                console.log ("fizzbuzz");
+            }
+            console.log( "fizz");
+        } else if(i%5 === 0) {
+            console.log( "buzz");
+        } else {
+            console.log(i);
         }
-        console.log("Fizz");
-    } else if(x % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log(i);
     }
 }
 
-for(i = 1; i <= 100; i++) {
-    fizzbuzz(i);
-}
+fizzBuzz(100);
